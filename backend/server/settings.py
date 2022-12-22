f04ab90b4661ad32f1fec037f686e24b11780323
Alex De Michieli <alexdemichieli@Alexs-MacBook-Pro.local>
2022-12-22 10:12:59 -0800

    pushing

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "j+qxsyi2xv!6lkv-hn)c$qxeo+t#1r#)0s)o^yh)ds#k%%19tm"
 
 # 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(os.environ.get("DEBUG", default=0))
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0']
